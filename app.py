a3ad6de07cd4cd0edf0fd4c76e306afb8152c59c
Jeff <jeffbohn@my.cwi.edu>
2025-03-04 09:27:22 -0700

    Change app.py

--- app.py
+++ app.py
@@ -2,13 +2,8 @@
 # 3/4/2025
 # Test App for Docker
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
 def hello():
-    return "Hello from Docker!"
+    print("Hello World")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    hello()
